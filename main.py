3d47043d16db84e371e5b87a1398aafd98f61a56
Ben Green <17412026+Pathfinder216@users.noreply.github.com>
2020-02-22 09:33:40 -0500

    Add material style with RUF colors

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
 
+import resources
+
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
 
